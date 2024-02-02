cf2918ef48d1f97eaa6cec9b0fe26cff7c04f79c
budayyy <budibuday97@gmail.com>
2024-02-02 19:13:07 +0700

    day #4

---
+++ 4-smash.js
@@ -0,0 +1,11 @@
+//2024-CODING-CHALLENGE
+// 4/366
+//https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+// function smash(words) {
+//   return words.join(" ");
+// }
+
+const smash = (words) => words.join(" ");
+
+console.log(smash(["hello", "world"]));
