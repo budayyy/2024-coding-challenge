6249d7422cdcbe2efdfa7b173973804e78d74d91
budayyy <budibuday97@gmail.com>
2024-02-05 17:18:51 +0700

    day #7

---
+++ 7-countBy.js
@@ -0,0 +1,18 @@
+//2024-CODING-CHALLENGE
+// 6/366
+//https://www.codewars.com/kata/5513795bd3fafb56c200049e
+
+// cara bar bar
+// function countBy(x, n) {
+//   let z = [];
+//   for (i = 1; i <= n; i++) {
+//     z.push(i * x);
+//   }
+//   return z;
+// }
+
+// cara one liner
+const countBy = (x, n) => [...Array(n)].map((_, i) => (i + 1) * x);
+
+console.log(countBy(1, 10));
+console.log(countBy(2, 5));
