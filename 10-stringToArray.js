2b4cebb922e853aa1eed026fae1c221443ebd353
budayyy <budibuday97@gmail.com>
2024-02-08 21:27:19 +0700

    day #10

---
+++ 10-stringToArray.js
@@ -0,0 +1,12 @@
+//2024-CODING-CHALLENGE
+// 10/366
+//https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/
+
+// function stringToArray(string) {
+//   // code code code
+//   return string.split(" ");
+// }
+
+const stringToArray = (string) => string.split(" ");
+
+console.log(stringToArray("Robin Singh"));
