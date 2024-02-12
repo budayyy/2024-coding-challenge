66ab1c9d1a0a52f60e58f563273253f01f0bd3b6
budayyy <budibuday97@gmail.com>
2024-02-12 19:59:49 +0700

    day #14

---
+++ 14-evenOrOdd.js
@@ -0,0 +1,13 @@
+//2024-CODING-CHALLENGE
+// 14/366
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(evenOrOdd(3));
