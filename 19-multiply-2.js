c5bbfc20da871755b806ea1b3402561f83345f26
budayyy <budibuday97@gmail.com>
2024-02-19 21:12:57 +0700

    day #19

---
+++ 19-multiply-2.js
@@ -0,0 +1,11 @@
+//2024-CODING-CHALLENGE
+// 19/366
+//https://www.codewars.com/kata/50654ddff44f800200000004
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(1, 1));
