0a491e4cc5bcda5c2516dbb1b643d2212bfed4e2
budayyy <budibuday97@gmail.com>
2024-02-22 21:21:32 +0700

    day #22

---
+++ 22-stringToNumber.js
@@ -0,0 +1,10 @@
+//2024-CODING-CHALLENGE
+// 22/366
+//https://www.codewars.com/kata/544675c6f971f7399a000e79
+
+const stringToNumber = function (str) {
+  // put your code here
+  return parseInt(str);
+};
+
+console.log(stringToNumber("1234"));
