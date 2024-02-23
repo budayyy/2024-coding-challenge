371752283a5807237ffa59bf6cf1e8e976085b9b
budayyy <budibuday97@gmail.com>
2024-02-23 21:10:21 +0700

    day #23

---
+++ 23-makeNegative.js
@@ -0,0 +1,10 @@
+//2024-CODING-CHALLENGE
+// 23/366
+//https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+function makeNegative(num) {
+  // Code?
+  return num > 0 ? -num : num;
+}
+
+console.log(makeNegative(42));
