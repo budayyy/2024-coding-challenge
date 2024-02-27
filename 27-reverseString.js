90a7742496862926bf12a9e2dc5096d60171796a
budayyy <budibuday97@gmail.com>
2024-02-27 20:24:14 +0700

    day #27

---
+++ 27-reverseString.js
@@ -0,0 +1,9 @@
+//2024-CODING-CHALLENGE
+// 27/366
+//https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+
+function solution(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(solution("world"));
