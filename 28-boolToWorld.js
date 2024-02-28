fe2d811c6a06aa98a1e9e835ea4eac015a59b407
budayyy <budibuday97@gmail.com>
2024-02-28 19:31:44 +0700

    day #28

---
+++ 28-boolToWorld.js
@@ -0,0 +1,9 @@
+//2024-CODING-CHALLENGE
+// 28/366
+//https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+function boolToWord(bool) {
+  return bool ? "Yes" : "No";
+}
+
+console.log(boolToWord(true));
