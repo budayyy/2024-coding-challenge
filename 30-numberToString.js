d08a80198cbb1b0448345bd4a927e6db44093e1f
budayyy <budibuday97@gmail.com>
2024-03-01 13:14:31 +0700

    day #30

---
+++ 30-numberToString.js
@@ -0,0 +1,10 @@
+//2024-CODING-CHALLENGE
+// 30/366
+//https://www.codewars.com/kata/5715eaedb436cf5606000381
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return num.toString();
+}
+
+console.log(numberToString(67));
