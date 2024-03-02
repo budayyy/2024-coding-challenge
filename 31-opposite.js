8f12511e3259666cd1a01429a56f8a270a868f0d
budayyy <budibuday97@gmail.com>
2024-03-02 12:05:38 +0700

    day #31

---
+++ 31-opposite.js
@@ -0,0 +1,10 @@
+//2024-CODING-CHALLENGE
+// 31/366
+//https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+function opposite(number) {
+  //your code here
+  return -number;
+}
+
+console.log(opposite(-34));
