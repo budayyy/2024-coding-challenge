143884e521fcd69a5a5ef36a8b83c8b343a80cbc
budayyy <budibuday97@gmail.com>
2024-03-04 20:15:56 +0700

    day #33

---
+++ 33-removeChar.js
@@ -0,0 +1,10 @@
+//2024-CODING-CHALLENGE
+// 33/366
+//https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+
+function removeChar(str) {
+  //You got this!
+  return str.slice(1, -1);
+}
+
+console.log(removeChar("eloquent"));
