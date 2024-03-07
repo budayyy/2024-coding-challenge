343de7f7fcf3167df76d154fe6317b8915be4ac9
budayyy <budibuday97@gmail.com>
2024-03-07 20:10:18 +0700

    day #36

---
+++ 36-noSpace.js
@@ -0,0 +1,9 @@
+//2024-CODING-CHALLENGE
+// 36/366
+//https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+function noSpace(x) {
+  return x.split(" ").join("");
+}
+
+console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
