3eeafe00322513fd123bc9b58c279c90f219407f
budayyy <budibuday97@gmail.com>
2024-03-10 14:07:09 +0700

    day #39

---
+++ 8kyu/39-century.js
@@ -0,0 +1,10 @@
+//2024-CODING-CHALLENGE
+// 39/366
+//https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+
+function century(year) {
+  // Finish this :)
+  return Math.ceil(year / 100);
+}
+
+console.log(century(2000));
