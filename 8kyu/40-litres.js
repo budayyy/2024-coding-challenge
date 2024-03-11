8962aea2254cbc0446bbc9d961c904db0a5b7954
budayyy <budibuday97@gmail.com>
2024-03-11 11:49:24 +0700

    day #40

---
+++ 8kyu/40-litres.js
@@ -0,0 +1,9 @@
+//2024-CODING-CHALLENGE
+// 40/366
+//https://www.codewars.com/kata/582cb0224e56e068d800003c
+
+function litres(time) {
+  return Math.floor(time / 2);
+}
+
+console.log(litres(11.8));
