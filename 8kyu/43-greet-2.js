35722a6281a73bbab3b4d9375b1dcff21d95908e
budayyy <budibuday97@gmail.com>
2024-03-14 14:23:39 +0700

    day #43

---
+++ 8kyu/43-greet-2.js
@@ -0,0 +1,11 @@
+//2024-CODING-CHALLENGE
+// 43/366
+//https://www.codewars.com/kata/55a70521798b14d4750000a4/
+
+function greet(name) {
+  //your code here
+
+  return `Hello, ${name} how are you doing today?`;
+}
+
+console.log(greet("Ryan"));
