4f1bc462eb358f148b71a84210589d5cfd41bbbc
budayyy <budibuday97@gmail.com>
2024-03-21 10:18:16 +0700

    day #50

---
+++ 8kyu/50-booleanToString.js
@@ -0,0 +1,10 @@
+//2024-CODING-CHALLENGE
+// 50/366
+//https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+function booleanToString(b) {
+  //your code here
+  return b.toString();
+}
+
+console.log(booleanToString(true));
