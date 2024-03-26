bb47d72a8c5a8eaaa009b7e18359b9f4fda024ff
budayyy <budibuday97@gmail.com>
2024-03-26 21:31:09 +0700

    day #55

---
+++ 7kyu/55-descendingOrder.js
@@ -0,0 +1,16 @@
+//2024-CODING-CHALLENGE
+// 55/366
+// https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+
+function descendingOrder(n) {
+  //...
+  return Number(
+    n
+      .toString()
+      .split("")
+      .sort((a, b) => b - a)
+      .join("")
+  );
+}
+
+console.log(descendingOrder(202));
