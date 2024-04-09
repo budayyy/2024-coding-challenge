b323c8f69b50aab8a7c4d96edab56d0efaaf68e8
budayyy <budibuday97@gmail.com>
2024-04-10 05:45:43 +0700

    day #70

---
+++ 7kyu/70-solution.js
@@ -0,0 +1,10 @@
+//2024-CODING-CHALLENGE
+// 70/366
+// https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+
+function solution(str, ending) {
+  // TODO: complete
+  return str.endsWith(ending);
+}
+
+console.log(solution("abcde", "cde"));
