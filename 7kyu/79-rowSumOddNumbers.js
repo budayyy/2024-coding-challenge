6fef6a02aedba4414ba37005241fcad71a5f5a1a
budayyy <budibuday97@gmail.com>
2024-04-19 13:22:48 +0700

    day #79

---
+++ 7kyu/79-rowSumOddNumbers.js
@@ -0,0 +1,14 @@
+//2024-CODING-CHALLENGE
+// 79/366
+// https://www.codewars.com/kata/55fd2d567d94ac3bc9000064
+
+// function rowSumOddNumbers(n) {
+//   // TODO
+//   return n * n * n;
+// }
+
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
+
+console.log(rowSumOddNumbers(2));
