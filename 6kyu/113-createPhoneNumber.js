e4a3eab8c22b217204806a6f0eab9fdd67235e5a
budayyy <budibuday97@gmail.com>
2024-05-23 21:10:28 +0700

    day #113

---
+++ 6kyu/113-createPhoneNumber.js
@@ -0,0 +1,9 @@
+//2024-CODING-CHALLENGE
+// 113/366
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83
+
+function createPhoneNumber(numbers) {
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); //(123) 456-7890
