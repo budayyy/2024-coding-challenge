3211caa489b5e7b7bb143aa5ba6fed1c239a9ad3
Budi Setiawan <68804067+budayyy@users.noreply.github.com>
2024-06-30 17:50:24 +0700

    day #150

---
+++ 6kyu/150-validBraces.js
@@ -0,0 +1,11 @@
+//2024-CODING-CHALLENGE
+// 150/366
+//https://www.codewars.com/kata/5277c8a221e209d3f6000b56
+
+function validBraces(braces){
+  //TODO 
+  while(/\(\)|\[\]|\{\}/g.test(braces)){braces = braces.replace(/\(\)|\[\]|\{\}/g,"")}
+ return !braces.length;
+}
+
+console.log(valodBraces("[]"));
