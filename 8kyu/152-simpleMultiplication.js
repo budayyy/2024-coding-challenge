98350b937844aab4effe301e269ede234ff83b9a
budayyy <budibuday97@gmail.com>
2024-07-02 21:37:25 +0700

    day #152

---
+++ 8kyu/152-simpleMultiplication.js
@@ -0,0 +1,10 @@
+//2024-CODING-CHALLENGE
+// 152/366
+// https://www.codewars.com/kata/583710ccaa6717322c000105
+
+function simpleMultiplication(number) {
+  // your code........
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
+
+console.log(simpleMultiplication(2));
