6210de0b7479c207d22f30b55ec5a7f0925112ef
budayyy <budibuday97@gmail.com>
2024-07-05 21:11:20 +0700

    day #155

---
+++ 8kyu/155-check.js
@@ -0,0 +1,12 @@
+//2024-CODING-CHALLENGE
+// 155/366
+// https://www.codewars.com/kata/57cc975ed542d3148f00015b
+
+function check(a, x) {
+  // your code here
+  return a.includes(x);
+}
+
+console.log(check([66, 101], 66));
+console.log(check(["t", "e", "s", "t"], "e"));
+console.log(check(["what", "a", "great", "kata"], "kat"));
