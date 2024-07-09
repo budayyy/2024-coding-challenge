74ff300997ce837ae2d4daf9b53d4638ae9c9947
budayyy <budibuday97@gmail.com>
2024-07-09 20:53:11 +0700

    day #159

---
+++ 8kyu/159-makeUpperCase.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 159/366
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+function makeUpperCase(str) {
+  // Code here
+  return str.toUpperCase();
+}
+
+console.log(makeUpperCase("Hello"));
