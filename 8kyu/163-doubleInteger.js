4ca4b1f89cb5d72785ae691248b75a041c0adee2
budayyy <budibuday97@gmail.com>
2024-07-13 21:32:43 +0700

    day #163

---
+++ 8kyu/163-doubleInteger.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 163/366
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
+
+console.log(doubleInteger(2));
