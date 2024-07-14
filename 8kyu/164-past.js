23e612c3f1fb3ca761c500117d0e9caa4a2823be
budayyy <budibuday97@gmail.com>
2024-07-14 21:46:44 +0700

    day #164

---
+++ 8kyu/164-past.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 164/366
+// https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
+
+function past(h, m, s) {
+  //#Happy Coding! ^_^
+  return h * 3600000 + m * 60000 + s * 1000;
+}
+
+console.log(past(0, 1, 1));
