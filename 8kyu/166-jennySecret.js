ce8c986a8a13fe52ffb00408b5df792d52778091
budayyy <budibuday97@gmail.com>
2024-07-16 20:32:16 +0700

    day #166

---
+++ 8kyu/166-jennySecret.js
@@ -0,0 +1,17 @@
+// 2024-CODING-CHALLENGE
+// 166/366
+// https://www.codewars.com/kata/576bb71bbbcf0951d5000044
+
+// function greet(name) {
+//   return "Hello, " + name + "!";
+//   if (name === "Johnny") return "Hello, my love!";
+// }
+
+// debugging
+function greet(name) {
+  if (name === "Johnny") {
+    return "Hello, my love!";
+  } else {
+    return "Hello, " + name + "!";
+  }
+}
