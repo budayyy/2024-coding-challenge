392f85d98859a3271ceec53f40faaf94a90a75ba
budayyy <budibuday97@gmail.com>
2024-07-17 22:00:48 +0700

    day #167

---
+++ 8kyu/167-getAge.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 167/366
+// https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1
+
+function getAge(inputString) {
+  // return the girl's correct age as an integer. Happy coding :)
+  return Number(inputString.split(" ")[0]);
+}
+
+console.log(getAge("4 years old"));
