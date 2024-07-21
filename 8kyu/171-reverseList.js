5021e80e6bb150c2a7f10cdd68ad065bbe9f59fa
budayyy <budibuday97@gmail.com>
2024-07-21 11:36:41 +0700

    day #171

---
+++ 8kyu/171-reverseList.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 171/366
+// https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b
+
+function reverseList(list) {
+  return list.reverse();
+}
+
+console.log(reverseList([1, 2, 3, 4]));
