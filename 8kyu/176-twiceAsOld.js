7278ae87c363b43afda0c4c1668e0bb91aa6d640
budayyy <budibuday97@gmail.com>
2024-07-26 22:12:29 +0700

    day #176

---
+++ 8kyu/176-twiceAsOld.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 176/366
+// https://www.codewars.com/kata/5b853229cfde412a470000d0
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  // your code here
+  return Math.abs(dadYearsOld - sonYearsOld * 2);
+}
+
+console.log(twiceAsOld(36, 7));
