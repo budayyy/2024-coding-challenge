5bdcc71f598ca706edadf02e37e0b2985c4b9cf5
budayyy <budibuday97@gmail.com>
2024-08-03 20:26:07 +0700

    day #183

---
+++ 8kyu/183-hoopCount.js
@@ -0,0 +1,14 @@
+// 2024-CODING-CHALLENGE
+// 183/366
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+
+function hoopCount(n) {
+  //your code goes here
+  if (n >= 10) {
+    return "Great, now move on to tricks";
+  } else {
+    return "Keep at it until you get it";
+  }
+}
+
+console.log(hoopCount(11));
