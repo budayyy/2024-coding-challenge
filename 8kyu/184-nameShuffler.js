7138b4ea5bf2cd4f1d8fd5fd7c5f0805556c5761
budayyy <budibuday97@gmail.com>
2024-08-04 20:16:35 +0700

    day #184

---
+++ 8kyu/184-nameShuffler.js
@@ -0,0 +1,11 @@
+// 2024-CODING-CHALLENGE
+// 184/366
+// https://www.codewars.com/kata/559ac78160f0be07c200005a
+
+function nameShuffler(str) {
+  //Shuffle It
+
+  return str.split(" ").reverse().join(" ");
+}
+
+console.log(nameShuffler("john McClane"));
