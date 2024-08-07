f0ce33cd3293a4189a6e17159e4f8c0445d47b06
budayyy <budibuday97@gmail.com>
2024-08-07 20:29:55 +0700

    day #187

---
+++ 8kyu/187-sumStr.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 187/366
+// https://www.codewars.com/kata/5966e33c4e686b508700002d/
+
+function sumStr(a, b) {
+  return `${Number(a) + Number(b)}`;
+}
+
+console.log(sumStr("4", "5"));
