bbfdef96d50e9a3335f72aa9ed1679994c7f5e47
budayyy <budibuday97@gmail.com>
2024-08-14 22:25:36 +0700

    day #194

---
+++ 8kyu/194-nearestSq.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 194/366
+// https://www.codewars.com/kata/5a805d8cafa10f8b930005ba
+
+function nearestSq(n) {
+  // your code
+  return Math.pow(Math.round(Math.sqrt(n)), 2);
+}
+
+console.log(nearestSq(1));
