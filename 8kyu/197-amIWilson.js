6dd9853e2cadb43f6b925a64aa0c026a72a56df1
budayyy <budibuday97@gmail.com>
2024-08-17 21:14:51 +0700

    day #197

---
+++ 8kyu/197-amIWilson.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 197/366
+// https://www.codewars.com/kata/55dc4520094bbaf50e0000cb/
+
+function amIWilson(p) {
+  return [5, 13, 563].indexOf(p) > -1;
+}
+
+console.log(amIWilson(563));
