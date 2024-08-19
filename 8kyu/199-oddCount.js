66d9eef63d944af4beacce33765824a7e3b0ac6b
budayyy <budibuday97@gmail.com>
2024-08-19 18:59:25 +0700

    day #199

---
+++ 8kyu/199-oddCount.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 199/366
+// https://www.codewars.com/kata/59342039eb450e39970000a6
+
+function oddCount(n) {
+  // your code here
+  return Math.floor(n / 2);
+}
+
+console.log(oddCount(15023));
