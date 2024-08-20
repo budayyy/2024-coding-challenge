ffa313c23e102ad1d8ce0c59e81b13ee7d5cdb13
budayyy <budibuday97@gmail.com>
2024-08-20 21:53:49 +0700

    day #200

---
+++ 8kyu/200-sayHello.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 200/366
+// https://www.codewars.com/kata/5625618b1fe21ab49f00001f
+
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
+
+console.log(sayHello("Mr. Spock"));
