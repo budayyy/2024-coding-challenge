7e91b2d2c5f028db14c51cb51bd7c1fe80a455f5
budayyy <budibuday97@gmail.com>
2024-08-21 22:14:36 +0700

    day #201

---
+++ 8kyu/201-cockroachSpeed.js
@@ -0,0 +1,11 @@
+// 2024-CODING-CHALLENGE
+// 201/366
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+
+function cockroachSpeed(s) {
+  //Good Luck!
+  let nilai = 27.7778;
+  return Math.floor(s * nilai);
+}
+
+console.log(cockroachSpeed(1.08));
