423287e5aba55bd5cfdf8ec1a52531f550dc32a8
budayyy <budibuday97@gmail.com>
2024-08-22 22:53:07 +0700

    day #202

---
+++ 8kyu/202-checkForFactor.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 202/366
+// https://www.codewars.com/kata/55cbc3586671f6aa070000fb
+
+function checkForFactor(base, factor) {
+  // code here
+  return base % factor === 0;
+}
+
+console.log(checkForFactor(10, 2));
