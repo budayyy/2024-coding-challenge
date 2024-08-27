0f64a4d9fff55b172f3f53466642db031c1a9d9b
budayyy <budibuday97@gmail.com>
2024-08-27 20:48:04 +0700

    day #207

--- 8kyu/207-switchItUp.js
+++ 8kyu/207-switchItUp.js
@@ -2,32 +2,46 @@
 // 207/366
 // https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
 
-function switchItUp(number) {
-  let word;
-  switch (number) {
-    case 0:
-      return (word = "Zero");
-    case 1:
-      return (word = "One");
-    case 2:
-      return (word = "Two");
-    case 3:
-      return (word = "Three");
-    case 4:
-      return (word = "Four");
-    case 5:
-      return (word = "Five");
-    case 6:
-      return (word = "Six");
-    case 7:
-      return (word = "Seven");
-    case 8:
-      return (word = "Eight");
-    case 9:
-      return (word = "Nine");
-  }
+// function switchItUp(number) {
+//   let word;
+//   switch (number) {
+//     case 0:
+//       return (word = "Zero");
+//     case 1:
+//       return (word = "One");
+//     case 2:
+//       return (word = "Two");
+//     case 3:
+//       return (word = "Three");
+//     case 4:
+//       return (word = "Four");
+//     case 5:
+//       return (word = "Five");
+//     case 6:
+//       return (word = "Six");
+//     case 7:
+//       return (word = "Seven");
+//     case 8:
+//       return (word = "Eight");
+//     case 9:
+//       return (word = "Nine");
+//   }
 
-  return word;
-}
+//   return word;
+// }
+
+const switchItUp = (n) =>
+  [
+    "Zero",
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+  ][n];
 
 console.log(switchItUp(2));
