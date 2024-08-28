647955f4b7b1a874f6a25b5d35a59f689f71580a
budayyy <budibuday97@gmail.com>
2024-08-28 21:10:48 +0700

    day #208

---
+++ 8kyu/208-warnTheSheep.js
@@ -0,0 +1,24 @@
+// 2024-CODING-CHALLENGE
+// 208/366
+// https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
+
+function warnTheSheep(queue) {
+  return queue.indexOf("wolf") === queue.length - 1
+    ? "Pls go away and stop eating my sheep"
+    : `Oi! Sheep number ${
+        queue.length - queue.indexOf("wolf") - 1
+      }! You are about to be eaten by a wolf!`;
+}
+
+console.log(
+  warnTheSheep([
+    "sheep",
+    "sheep",
+    "sheep",
+    "sheep",
+    "sheep",
+    "wolf",
+    "sheep",
+    "sheep",
+  ])
+);
