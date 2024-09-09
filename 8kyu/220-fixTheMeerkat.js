b1488c2adb670e4f994ae3a08d5b4bc3cb3a595b
budayyy <budibuday97@gmail.com>
2024-09-09 21:50:38 +0700

    day #220

---
+++ 8kyu/220-fixTheMeerkat.js
@@ -0,0 +1,11 @@
+// 2024-CODING-CHALLENGE
+// 220/366
+// https://www.codewars.com/kata/56f699cd9400f5b7d8000b55/
+
+function fixTheMeerkat(arr) {
+  //your code here
+
+  return arr.reverse();
+}
+
+console.log(fixTheMeerkat(["tail", "body", "head"]));
