f775a326bb688a59d746b430279b2888a7f2ec19
budayyy <budibuday97@gmail.com>
2024-09-15 08:18:15 +0700

    day #226

---
+++ 8kyu/226-nthEven.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 226/366
+// https://www.codewars.com/kata/5933a1f8552bc2750a0000ed
+
+function nthEven(n) {
+  // your code here
+  return n * 2 - 2;
+}
+
+console.log(nthEven(2));
