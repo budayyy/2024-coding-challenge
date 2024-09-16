0c0a087ba9a93aa5b7b1d11a75bc7db59fd380f4
budayyy <budibuday97@gmail.com>
2024-09-16 22:06:06 +0700

    day #227

---
+++ 8kyu/227-move.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 227/366
+// https://www.codewars.com/kata/563a631f7cbbc236cf0000c2
+
+function move(position, roll) {
+  // return the new position
+  return position + 2 * roll;
+}
+
+console.log(move(2, 5));
