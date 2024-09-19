522a00ba26a0e8f6b86b5ccfe884bf719fd30891
budayyy <budibuday97@gmail.com>
2024-09-19 22:43:41 +0700

    day #230

---
+++ 8kyu/230-replaceDots.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 230/366
+// https://www.codewars.com/kata/596c6eb85b0f515834000049/
+
+var replaceDots = function (str) {
+  return str.replaceAll(".", "-");
+};
+
+console.log(replaceDots("one.two.three"));
