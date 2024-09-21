693d74f162ddfc557b4208814aaf141784dff2aa
budayyy <budibuday97@gmail.com>
2024-09-21 21:56:39 +0700

    day #232

---
+++ 8kyu/232-getVolumeOfCuboid.js
@@ -0,0 +1,12 @@
+// 2024-CODING-CHALLENGE
+// 232/366
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    // your code here
+    return length * width * height;
+  }
+}
+
+console.log(Kata.getVolumeOfCuboid(6, 2, 5));
