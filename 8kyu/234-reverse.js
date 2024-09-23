88c7dd2fdc28e4c4d6ffc83468e81f7c76a3e88b
budayyy <budibuday97@gmail.com>
2024-09-23 13:18:10 +0700

    day #234

---
+++ 8kyu/234-reverse.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 234/366
+// https://www.codewars.com/kata/57a55c8b72292d057b000594
+
+function reverse(string) {
+  //your code here
+  return string.split(" ").reverse().join(" ");
+}
+
+console.log(reverse("I am an expert at this"));
