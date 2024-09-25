2f04b6103ec2c5b3bc15e68d4b137fadc1c7f1b5
budayyy <budibuday97@gmail.com>
2024-09-25 09:26:20 +0700

    day #236

---
+++ 8kyu/236-rainAmount.js
@@ -0,0 +1,13 @@
+// 2024-CODING-CHALLENGE
+// 236/366
+// https://www.codewars.com/kata/57158fb92ad763bb180004e7
+
+function rainAmount(mm) {
+  if (mm < 40) {
+    return `You need to give your plant ${40 - mm}mm of water`;
+  } else {
+    return `Your plant has had more than enough water for today!`;
+  }
+}
+
+console.log(rainAmount(39));
