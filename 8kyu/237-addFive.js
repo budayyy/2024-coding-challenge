76c03c67e7fdd356072dd94fe8875f8f0e94e572
budayyy <budibuday97@gmail.com>
2024-09-26 09:02:25 +0700

    day #237

---
+++ 8kyu/237-addFive.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 237/366
+// https://www.codewars.com/kata/56200d610758762fb0000002
+
+function addFive(num) {
+  var total = num + 5;
+  return total;
+}
+
+console.log(addFive(-5));
