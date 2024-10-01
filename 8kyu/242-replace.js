b856c432f7088043ca1fd35485babfb0632e12ed
budayyy <budibuday97@gmail.com>
2024-10-01 20:47:04 +0700

    day #242

---
+++ 8kyu/242-replace.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 242/366
+// https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed
+
+function replace(s) {
+  //coding and coding....
+  return s.replace(/[aiueoAIUEO]/g, "!");
+}
+
+console.log(replace("ABCDE"));
