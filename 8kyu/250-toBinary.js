b3cf07829691eafbf33ff2bd2055c9c69913d460
budayyy <budibuday97@gmail.com>
2024-10-09 10:36:08 +0700

    day #250

---
+++ 8kyu/250-toBinary.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 250/366
+// https://www.codewars.com/kata/59fca81a5712f9fa4700159a/
+
+function toBinary(n) {
+  return parseInt(n.toString(2));
+}
+
+console.log(toBinary(3));
