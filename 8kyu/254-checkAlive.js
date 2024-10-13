68cc5d7f085747c88bf4d1cde9a6268255ebeecf
budayyy <budibuday97@gmail.com>
2024-10-13 14:11:50 +0700

    day #254

---
+++ 8kyu/254-checkAlive.js
@@ -0,0 +1,15 @@
+// 2024-CODING-CHALLENGE
+// 254/366
+// https://www.codewars.com/kata/57089707fe2d01529f00024a
+
+// function checkAlive(health) {
+//   if (health > 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+const checkAlive = (health) => health > 0;
+
+console.log(checkAlive(5));
