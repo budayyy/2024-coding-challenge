ebb469f16be9181a07c71bb1b3b8a5c298b55180
budayyy <budibuday97@gmail.com>
2024-10-15 21:01:56 +0700

    day #256

---
+++ 8kyu/256-binToDec.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 256/366
+// https://www.codewars.com/kata/57a5c31ce298a7e6b7000334
+
+function binToDec(bin) {
+  // TODO
+  return parseInt(bin, 2);
+}
+
+console.log(binToDec("10"));
