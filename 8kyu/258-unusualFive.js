6fd49bc4e56c5129430b1db9be4c41b9f50cf028
budayyy <budibuday97@gmail.com>
2024-10-17 22:51:11 +0700

    day #258

---
+++ 8kyu/258-unusualFive.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 258/366
+// https://www.codewars.com/kata/59441520102eaa25260000bf
+
+function unusualFive() {
+  return "lima!".length;
+}
+
+console.log(unusualFive());
