a56e87f8e5cfa44e7f8330772d5372942d4ef4e3
budayyy <budibuday97@gmail.com>
2024-10-18 09:34:57 +0700

    day #259

---
+++ 8kyu/259-helloWorld.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 259/366
+// https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8
+
+function helloWorld() {
+  let str = "Hello World!";
+  console.log(str);
+}
+
+console.log(helloWorld());
