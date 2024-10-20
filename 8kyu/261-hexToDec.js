56353ff56a918919d140ca775b638c31f7db5549
budayyy <budibuday97@gmail.com>
2024-10-20 19:26:49 +0700

    day #261

---
+++ 8kyu/261-hexToDec.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 261/366
+// https://www.codewars.com/kata/57a4d500e298a7952100035d
+
+function hexToDec(hexString) {
+  //your code here
+  return parseInt(hexString, 16);
+}
+
+console.log(hexToDec("FF"));
