d3bbd1f648be73cc2f82d59670fc5a8236d661fa
budayyy <budibuday97@gmail.com>
2024-10-23 20:49:58 +0700

    day #264

---
+++ 8kyu/264-take.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 264/366
+// https://www.codewars.com/kata/545afd0761aa4c3055001386
+
+function take(arr, n) {
+  // Your code here
+  return arr.slice(0, n);
+}
+
+console.log(take([0, 1, 2, 3, 5, 8, 13], 3));
