09f09e6be31f98c0e4c550f9d7f74557e3584613
budayyy <budibuday97@gmail.com>
2024-10-27 19:20:11 +0700

    day #268

---
+++ 8kyu/268-getRealFloor.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 267/366
+// https://www.codewars.com/kata/574b3b1599d8f897470018f6
+
+function getRealFloor(n) {
+  return n > 13 ? n - 2 : n > 0 ? n - 1 : n;
+}
+
+console.log(getRealFloor(15));
