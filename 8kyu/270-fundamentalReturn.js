30b6b7e90fb43a3e75ca4de994a42b0787fcfe58
budayyy <budibuday97@gmail.com>
2024-10-29 19:31:51 +0700

    day #270

---
+++ 8kyu/270-fundamentalReturn.js
@@ -0,0 +1,34 @@
+// 2024-CODING-CHALLENGE
+// 270/366
+// https://www.codewars.com/kata/55a5befdf16499bffb00007b
+
+function add(a, b) {
+  return a + b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function mod(a, b) {
+  return a % b;
+}
+
+function exponent(a, b) {
+  return a ** b;
+}
+
+function subt(a, b) {
+  return a - b;
+}
+
+console.log(add(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
+console.log(mod(1, 2));
+console.log(exponent(1, 2));
+console.log(subt(1, 2));
