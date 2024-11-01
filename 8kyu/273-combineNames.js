f63a495b271467af475d80a4df24b5cf8e63bf13
budayyy <budibuday97@gmail.com>
2024-11-01 20:00:07 +0700

    day #273

---
+++ 8kyu/273-combineNames.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 273/366
+// https://www.codewars.com/kata/55f73f66d160f1f1db000059
+
+const combineNames = (first, last) => {
+  return `${first} ${last}`;
+};
+
+console.log(combineNames("James", "Stevens"));
