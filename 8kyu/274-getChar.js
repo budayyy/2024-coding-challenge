c63289bddae22ec89382eea075736a23ebc69ae5
budayyy <budibuday97@gmail.com>
2024-11-02 17:45:51 +0700

    day #274

---
+++ 8kyu/274-getChar.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 274/366
+// https://www.codewars.com/kata/55ad04714f0b468e8200001c
+
+function getChar(c) {
+  // ...
+  return String.fromCharCode(c);
+}
+
+console.log(getChar(55));
