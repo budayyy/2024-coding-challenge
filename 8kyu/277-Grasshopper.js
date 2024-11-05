87ec8ad19e9dc789eefac44d29088f6f9fc0a742
budayyy <budibuday97@gmail.com>
2024-11-05 19:43:22 +0700

    day #277

---
+++ 8kyu/277-Grasshopper.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 277/366
+// https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/
+
+function main(verb, noun) {
+  return verb + noun;
+}
+
+console.log(main("take ", "item"));
