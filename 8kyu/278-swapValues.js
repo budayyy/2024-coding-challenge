fc0622d6e8da3597320c1d161af6c3955a538fdd
budayyy <budibuday97@gmail.com>
2024-11-06 22:22:13 +0700

    day #278

---
+++ 8kyu/278-swapValues.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 278/366
+// https://www.codewars.com/kata/5388f0e00b24c5635e000fc6
+
+function swapValues() {
+  return arguments[0].reverse();
+}
+
+console.log(swapValues());
