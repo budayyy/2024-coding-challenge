7bf960950f6e7ec20802f5936704d74d777a40fd
budayyy <budibuday97@gmail.com>
2024-11-09 20:56:56 +0700

    day #281

---
+++ 8kyu/281-include.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 281/366
+// https://www.codewars.com/kata/545991b4cbae2a5fda000158
+
+function include(arr, item) {
+  // ...
+  return arr.includes(item);
+}
+
+console.log(include([1, 2, 3, 4], 3));
