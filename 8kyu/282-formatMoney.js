442fa41e3a4e28fac8cdf3991add5d5aa18b4c82
budayyy <budibuday97@gmail.com>
2024-11-10 22:43:40 +0700

    day #282

---
+++ 8kyu/282-formatMoney.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 282/366
+// https://www.codewars.com/kata/55902c5eaa8069a5b4000083
+
+function formatMoney(amount) {
+  // your formatting code here
+  return `$${amount.toFixed(2)}`;
+}
+
+console.log(formatMoney(3.99));
