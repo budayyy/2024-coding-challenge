dceb2302ed13333896cf9ecb4e8fb60778f29989
budayyy <budibuday97@gmail.com>
2024-11-13 23:24:42 +0700

    day #285

---
+++ 8kyu/285-check.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 285/366
+// https://www.codewars.com/kata/57cc40b2f8392dbf2a0003ce
+
+function check(a, x) {
+  return a.includes(x);
+}
+
+console.log(check([66, 101], 66));
