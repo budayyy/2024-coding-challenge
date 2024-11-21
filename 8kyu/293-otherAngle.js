4b5c0498105e0c8cc403b8756c2913e4c27007c7
budayyy <budibuday97@gmail.com>
2024-11-21 22:22:56 +0700

    day #293

---
+++ 8kyu/293-otherAngle.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 293/366
+// https://www.codewars.com/kata/5a023c426975981341000014
+
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
+
+console.log(otherAngle(30, 60));
