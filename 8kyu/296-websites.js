88ca11ef64fe8623703c71d824b086537a1021af
budayyy <budibuday97@gmail.com>
2024-11-24 19:05:45 +0700

    day #296

---
+++ 8kyu/296-websites.js
@@ -0,0 +1,11 @@
+// 2024-CODING-CHALLENGE
+// 296/366
+// https://www.codewars.com/kata/525c1a07bb6dda6944000031
+
+var websites = [];
+
+for (let index = 0; index < 1000; index++) {
+  websites.push("codewars");
+}
+
+console.log(websites);
