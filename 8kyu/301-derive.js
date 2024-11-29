c230b2c534e4bdd480e3214ba9eb2bd8de497121
budayyy <budibuday97@gmail.com>
2024-11-29 21:52:01 +0700

    day #301

---
+++ 8kyu/301-derive.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 301/366
+// https://www.codewars.com/kata/5963c18ecb97be020b0000a2
+
+function derive(coefficient, exponent) {
+  return `${coefficient * exponent}x^${exponent - 1}`;
+}
+
+console.log(derive(7, 8));
