97ebf7f3870b9f3011dfdaac86e147a312f96a2c
budayyy <budibuday97@gmail.com>
2024-12-05 20:50:56 +0700

    day #307

---
+++ 8kyu/307-twoDecimalPlaces.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 307/366
+// https://www.codewars.com/kata/5641a03210e973055a00000d
+
+function twoDecimalPlaces(n) {
+  // Your code here
+  return parseFloat(n.toFixed(2));
+}
+
+console.log(twoDecimalPlaces(4.659725356));
