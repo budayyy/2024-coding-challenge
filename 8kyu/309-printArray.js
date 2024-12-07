10e55b9be2e1b5576b7caa44ebe9315b4a09e86b
budayyy <budibuday97@gmail.com>
2024-12-07 22:34:39 +0700

    day #309

---
+++ 8kyu/309-printArray.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 309/366
+// https://www.codewars.com/kata/5a360620f28b82a711000047
+
+function printArray(array) {
+  //show me the code!
+  return array.join();
+}
+
+console.log(printArray(["h", "o", "l", "a"]));
