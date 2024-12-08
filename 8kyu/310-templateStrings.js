7d4e5dc093241aa7d7e7ee5fb013b3d70ce37915
budayyy <budibuday97@gmail.com>
2024-12-08 19:54:27 +0700

    day #310

---
+++ 8kyu/310-templateStrings.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 310/366
+// https://www.codewars.com/kata/55a14f75ceda999ced000048
+
+var templateStrings = function (noun, adjective) {
+  return `${noun} are ${adjective}`;
+};
+
+console.log(templateStrings("Animals", "Good"));
