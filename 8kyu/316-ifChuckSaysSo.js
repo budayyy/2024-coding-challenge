9b2c198fb29915bcf2f19db5aac2c93a1937c660
budayyy <budibuday97@gmail.com>
2024-12-14 21:03:22 +0700

    day #316

---
+++ 8kyu/316-ifChuckSaysSo.js
@@ -0,0 +1,9 @@
+// 2024-CODING-CHALLENGE
+// 316/366
+// https://www.codewars.com/kata/570669d8cb7293a2d1001473
+
+function ifChuckSaysSo() {
+  return NaN == NaN;
+}
+
+console.log(ifChuckSaysSo());
