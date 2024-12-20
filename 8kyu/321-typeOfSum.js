d685152fd0ad2518c8d6773f6cf11dd58842d08c
budayyy <budibuday97@gmail.com>
2024-12-20 23:06:15 +0700

    day #321

---
+++ 8kyu/321-typeOfSum.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 321/366
+// https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba
+
+function typeOfSum(a, b) {
+  // good luck
+  return typeof (a + b);
+}
+
+console.log(typeOfSum(12, 1));
