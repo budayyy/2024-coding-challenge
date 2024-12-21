cc2d9e8aef518a2562f44e8b4dbdf5f4f1253cdd
budayyy <budibuday97@gmail.com>
2024-12-21 21:48:52 +0700

    day #322

---
+++ 8kyu/322-howManyLightsabersDoYouOwn.js
@@ -0,0 +1,13 @@
+// 2024-CODING-CHALLENGE
+// 322/366
+// https://www.codewars.com/kata/51f9d93b4095e0a7200001b8
+
+function howManyLightsabersDoYouOwn(name) {
+  let result = 0;
+
+  if (name == "Zach") return 18;
+
+  return result;
+}
+
+console.log(howManyLightsabersDoYouOwn("Zach"));
