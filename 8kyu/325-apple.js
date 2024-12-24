e60b863b6f2f415ed3a3a57cdd81b5d89452b707
budayyy <budibuday97@gmail.com>
2024-12-24 21:47:27 +0700

    day #325

---
+++ 8kyu/325-apple.js
@@ -0,0 +1,11 @@
+// 2024-CODING-CHALLENGE
+// 325/366
+// https://www.codewars.com/kata/580a094553bd9ec5d800007d
+
+function apple(x) {
+  return Math.pow(x, 2) > 1000
+    ? "It's hotter than the sun!!"
+    : "Help yourself to a honeycomb Yorkie for the glovebox.";
+}
+
+console.log(apple("50"));
