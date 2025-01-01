f5531eac2ea70ba81bd8f84cc97bb38ae0569ec1
budayyy <budibuday97@gmail.com>
2025-01-01 21:57:34 +0700

    day #333

---
+++ 8kyu/333-getASCII.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 333/366
+// https://www.codewars.com/kata/55acfc59c3c23d230f00006d
+
+function getASCII(c) {
+  // ...
+  return c.charCodeAt();
+}
+
+console.log(getASCII("A"), 65);
