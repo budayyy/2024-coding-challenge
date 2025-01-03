ac2497907d2da2f66335df40c16fcac117d88911
budayyy <budibuday97@gmail.com>
2025-01-03 20:39:50 +0700

    day #335

---
+++ 8kyu/335-joinStrings.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 335/366
+// https://www.codewars.com/kata/582e4c3406e37fcc770001ad
+
+function joinStrings(string1, string2) {
+  // code here
+  return `${string1} ${string2}`;
+}
+
+console.log(joinStrings("string1", "string2"));
