54f6a71c924a8f786e3150c8470d02536833de78
budayyy <budibuday97@gmail.com>
2025-01-04 20:36:46 +0700

    day #336

---
+++ 6kyu/336-remove.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 336/366
+// https://www.codewars.com/kata/57faf12b21c84b5ba30001b0
+
+function remove(string) {
+  //coding and coding....
+  return string.replace(/!/g, "") + "!";
+}
+
+console.log(remove("Hi!"));
