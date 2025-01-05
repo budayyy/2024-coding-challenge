c50fcddcc627cb07d0684ac615736026bdb62cf6
budayyy <budibuday97@gmail.com>
2025-01-05 20:46:31 +0700

    day #337

---
+++ 8kyu/337-shortenToDate.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 337/366
+// https://www.codewars.com/kata/56b0ff16d4aa33e5bb00008e
+
+function shortenToDate(longDate) {
+  // your code here
+  return longDate.split(",")[0];
+}
+
+console.log(shortenToDate("Friday May 2, 9am"));
