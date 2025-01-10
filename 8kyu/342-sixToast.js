8dcc1fb32699a4e43d64715a2f12edbdbd8d0166
budayyy <budibuday97@gmail.com>
2025-01-10 22:03:04 +0700

    day #342

---
+++ 8kyu/342-sixToast.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 342/366
+// https://www.codewars.com/kata/5834fec22fb0ba7d080000e8
+
+function sixToast(num) {
+  // you code here
+  return Math.abs(num - 6);
+}
+
+console.log(sixToast(6));
