a275aaea3bb20edd1be89f64d6d39ce77d7f1568
budayyy <budibuday97@gmail.com>
2025-01-20 20:19:23 +0700

    day #352

---
+++ 8kyu/352-add.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 352/366
+// https://www.codewars.com/kata/57f6ecdfcca6e045d2001207
+
+function add(a, b) {
+  // code here
+  return a == b;
+}
+
+console.log(add("1", 1));
