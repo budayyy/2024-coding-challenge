66b61c6e104ad554b78dc061a84525950b05f101
budayyy <budibuday97@gmail.com>
2025-01-29 22:07:02 +0700

    day #361

---
+++ 8kyu/361-sakuraFall.js
@@ -0,0 +1,10 @@
+// 2024-CODING-CHALLENGE
+// 361/366
+// https://www.codewars.com/kata/5a0be7ea8ba914fc9c00006b
+
+function sakuraFall(v) {
+  // your code here
+  return v <= 0 ? 0 : 400 / v;
+}
+
+console.log(sakuraFall(5));
